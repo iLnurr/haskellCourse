7462c2b62c4f8e71b282e9bd0f2591c5a74a8434
ilnur serbaev <serbaevilnur@gmail.com>
2018-03-09 15:54:26 +0500

    fix ambigious occurense

--- eq.hs
+++ eq.hs
@@ -1,5 +1,12 @@
 module Demo where
 
-class Eq a where
-  (==) :: a -> a -> Bool
-  (/=) :: a -> a -> Bool
+class EqI a where
+  (===) :: a -> a -> Bool
+  (/==) :: a -> a -> Bool
+
+instance EqI Bool where
+  True  === True   =  True
+  False === False  =  True
+  _     === _      =  False
+
+  x /== y  = not (x === y)
