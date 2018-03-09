47994cde06807ce37d9e80c7132d27939309b8cf
ilnur serbaev <serbaevilnur@gmail.com>
2018-03-09 15:55:35 +0500

    default method

--- eq.hs
+++ eq.hs
@@ -3,10 +3,9 @@ module Demo where
 class EqI a where
   (===) :: a -> a -> Bool
   (/==) :: a -> a -> Bool
+  x /== y  = not (x === y)
 
 instance EqI Bool where
   True  === True   =  True
   False === False  =  True
   _     === _      =  False
-
-  x /== y  = not (x === y)
