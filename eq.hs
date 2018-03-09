db53170a3733cf5bea9d59e87a3448d4d3e05461
ilnur serbaev <serbaevilnur@gmail.com>
2018-03-09 16:28:43 +0500

    EqI instance for tuples

--- eq.hs
+++ eq.hs
@@ -4,8 +4,12 @@ class EqI a where
   (===) :: a -> a -> Bool
   (/==) :: a -> a -> Bool
   x /== y  = not (x === y)
+  x === y  = not (x /== y)
 
 instance EqI Bool where
   True  === True   =  True
   False === False  =  True
   _     === _      =  False
+
+instance (EqI a, EqI b) => EqI (a, b) where
+  p1 === p2  =  fst p1 === fst p2 && snd p1 === snd p2
