d15bf84fb963445b36ff94b295a261daa88ec0f8
ilnur <ilnur.serbaev@dz.ru>
2018-03-14 21:43:53 +0500

    avg

--- test.hs
+++ test.hs
@@ -1,3 +1,6 @@
 module Test where
 
 sayHello = putStrLn "Hello world from module Test!"
+
+avg :: Int -> Int -> Int -> Double
+avg a b c = ((fromIntegral a) + (fromIntegral b) + (fromIntegral c)) / 3
