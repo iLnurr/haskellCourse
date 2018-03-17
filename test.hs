e2106038ab8e598bf71b853592692f52a6dd7c1f
ilnur serbaev <serbaevilnur@gmail.com>
2018-03-17 14:26:24 +0500

    форсирование вычислений

--- test.hs
+++ test.hs
@@ -4,3 +4,10 @@ sayHello = putStrLn "Hello world from module Test!"
 
 avg :: Int -> Int -> Int -> Double
 avg a b c = ((fromIntegral a) + (fromIntegral b) + (fromIntegral c)) / 3
+
+factorial :: Integer -> Integer
+factorial n  | n >= 0   = helper 1 n
+             | otherwise = error "arg must be >= 0"
+   where
+     helper acc 0 = acc
+     helper acc n = (helper $! (acc * n)) (n - 1)
