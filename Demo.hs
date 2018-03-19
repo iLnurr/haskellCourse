fa734f9043aff046c8ef1b19e2a76ae74215f19f
ilnur serbaev <serbaevilnur@gmail.com>
2018-03-19 09:41:33 +0500

    modules

---
+++ Demo.hs
@@ -0,0 +1,18 @@
+module Demo where
+
+import Data.Char hiding (toLower)
+
+import Data.List hiding (union)
+
+--import Data.Set
+import qualified Data.Set as Set
+
+{-
+:t union
+
+:t Set.union
+
+-}
+
+--myUnion [] ys = ys
+--myUnion xs ys = union xs ys
