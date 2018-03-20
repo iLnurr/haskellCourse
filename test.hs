9d6bebedd068828dcee3754a435298c76f4b35b3
ilnur serbaev <serbaevilnur@gmail.com>
2018-03-20 10:45:46 +0500

    List #1

--- test.hs
+++ test.hs
@@ -26,3 +26,61 @@ factorial n  | n >= 0   = helper 1 n
   5.2. код СТГ машины -> код С--
 6. генерация либо код для целевой машины либо код LLVM
 -}
+
+{-
+
+Lists
+[] - new List
+
+3 : [] - add 3 to head of empty list
+
+[5,3] == 5 : 3 : []
+
+let cons42 = (42 :)
+
+cons42 [1,2,3]
+
+**
+addTwoElements :: a -> a -> [a] -> [a]
+addTwoElements a b c= a : b : c
+
+**
+Реализуйте функцию nTimes, которая возвращает список, состоящий из повторяющихся значений ее первого аргумента. Количество повторов определяется значением второго аргумента этой функции.
+nTimes:: a -> Int -> [a]
+nTimes a b = helper a b []
+  where
+    helper a 0 acc = acc
+    helper a b acc = helper a (b - 1) (a : acc)
+
+
+head :: [a] -> a
+head [1,2,3] = 1
+
+tail :: [a] -> [a]
+tail [1,2,3] -> [2,3]
+
+second :: [a] -> a
+let second xs = head (tail xs)
+let second = head . tail
+
+let fst' ((,) x y) = x   - сопост авление с образцом для пары в префиксном стиле
+
+let head' ((:) x xs) = x - сопоставление с образцом для списка в префиксном стиле
+
+let head' (x : xs) = x - сопоставление с образцом для списка в инфиксном стиле
+
+second' :: [a] -> a
+let second' (_ : xs) = head xs - !!!
+
+second'' :: [a] -> a
+let second'' (_ : x : _) = x
+
+sndHead :: [(a, c)] -> c
+let sndHead = snd . head
+
+**
+Сформируйте список целых чисел, содержащий только те элементы исходного списка, значение которых нечетно.
+oddsOnly :: Integral a => [a] -> [a]
+oddsOnly a = filter odd a
+
+-}
