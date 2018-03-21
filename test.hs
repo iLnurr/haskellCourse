32891b9aa8b984058ccbd2d4428e15ab3d0cc006
ilnur serbaev <serbaevilnur@gmail.com>
2018-03-21 12:43:55 +0500

    List #4

--- test.hs
+++ test.hs
@@ -232,6 +232,28 @@ squares'n'cubes :: Num a => [a] -> [a]
 squares'n'cubes xs = concatMap (\x -> [x * x, x * x * x]) xs
 **
 
+**
+Воспользовавшись функциями map и concatMap, определите функцию perms, которая возвращает все перестановки, которые можно получить из данного списка, в любом порядке.
+
+GHCi> perms [1,2,3]
+[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Считайте, что все элементы в списке уникальны, и что для пустого списка имеется одна перестановка.
+
+perms :: Eq a => [a] -> [[a]]
+perms [] = []
+perms xs@(x:xs') = concatMap (\x -> helper x (filter (x /=) xs)) xs
+  where
+    helper x []     = [[x]]
+    helper x [y]  = [[x,y],[y,x]]
+    helper x [y,z]  = [[x,y,z],[x,z,y],[y,x,z],[y,z,x],[z,x,y],[z,y,x]]
+    helper x (y:ys) = map (\a -> x:a) (helper y ys)
+
+perms :: [a] -> [[a]]
+perms [] = [[]]
+perms [x] = [[x]]
+perms (x:xs) = concatMap (insertElem x) (perms xs) where
+			insertElem x [] = [[x]]
+			insertElem x yss@(y:ys) = (x:yss) : map (y:) (insertElem x ys)
 
 
 -}
