dd92fbc8a12d9ec2efa8c412ec5caeb7389e5738
ilnur serbaev <serbaevilnur@gmail.com>
2018-03-22 10:46:37 +0500

    List #7

--- test.hs
+++ test.hs
@@ -341,7 +341,6 @@ let squares = map (^2) $ nats 1
 
 **
 Реализуйте c использованием функции zipWith функцию fibStream, возвращающую бесконечный список чисел Фибоначчи.
-
 GHCi> take 10 $ fibStream
 [0,1,1,2,3,5,8,13,21,34]
 
@@ -353,7 +352,7 @@ fibSt _ [] = []
 fibSt xs ys = xs ++ ys ++ tail (fibSt ys (tail (helper xs ys)))
   where
     helper a b = a ++ (zipWith (+) a b)
-
+**
 
 
 
