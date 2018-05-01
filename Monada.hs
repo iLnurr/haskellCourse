8b42d3288d9d92e03cd0de8b0b240a441a7862f4
ilnur <ilnur.serbaev@dz.ru>
2018-05-01 14:04:25 +0500

    Monad

--- Monada.hs
+++ Monada.hs
@@ -15,7 +15,154 @@ f :: a -> (State s) b   -- работают с изменяемым состоя
 f :: a -> IO b          -- осуществляют ввод-вывод
 
 Обобщая, получим стрелку Клейсли:
-
 f :: a -> m b
 
+
+class Monad m where
+  return :: a -> m a
+  (>>=) :: m a -> (a -> m b) -> m b -- произносят bind
+  ...
+infixl 1 >>=
+
+
+Prelude> :t return True
+return True :: Monad m => m Bool
+Prelude> return True :: [Bool]
+[True]
+Prelude> return True :: Maybe Bool
+Just True
+
+
+toKleisli :: Monad m => (a -> b) -> (a -> m b)
+toKleisli f = \x -> return (f x) -- or toKleisli f x = return (f x)
+
+
+**
+Введём следующий тип:
+
+data Log a = Log [String] a
+
+Реализуйте вычисление с логированием, используя Log. Для начала определите функцию toLogger
+
+toLogger :: (a -> b) -> String -> (a -> Log b)
+
+которая превращает обычную функцию, в функцию с логированием:
+
+GHCi> let add1Log = toLogger (+1) "added one"
+GHCi> add1Log 3
+Log ["added one"] 4
+
+GHCi> let mult2Log = toLogger (* 2) "multiplied by 2"
+GHCi> mult2Log 3
+Log ["multiplied by 2"] 6
+
+Далее, определите функцию execLoggers
+
+execLoggers :: a -> (a -> Log b) -> (b -> Log c) -> Log c
+
+
+Которая принимает некоторый элемент и две функции с логированием. execLoggers возвращает результат последовательного применения функций к элементу и список сообщений, которые были выданы при применении каждой из функций:
+
+GHCi> execLoggers 3 add1Log mult2Log
+Log ["added one","multiplied by 2"] 8
+
+toLogger :: (a -> b) -> String -> (a -> Log b)
+toLogger f xs v = Log [xs] (f v)
+
+execLoggers :: a -> (a -> Log b) -> (b -> Log c) -> Log c
+execLoggers v f g =  Log (xs ++ xss)  v'' where
+                     Log xs v' = f v
+                     Log xss v'' = g v'
+**
+
+**
+Функции с логированием из предыдущего задания возвращают в качестве результата значение с некоторой дополнительной информацией в виде списка сообщений. Этот список является контекстом. Реализуйте функцию returnLog
+
+returnLog :: a -> Log a
+
+
+которая является аналогом функции return для контекста Log. Данная функция должна возвращать переданное ей значение с пустым контекстом.
+
+returnLog :: a -> Log a
+returnLog a = Log [] a
+**
+
+-- Bind
+infixr 0 $
+($) :: (a -> b) -> a -> b
+f $ x = f x
+
+infixl 1 &
+(&) :: a -> (a -> b) -> b
+x & f = f x
+
+Prelude> (+1) $ (*3) $ (+2) $ 5 -- ((5 + 2) * 3) + 1
+22
+Prelude> 5 & (+2) & (*3) & (+1) -- ((5 + 2) * 3) + 1
+22
+
+-- синтаксический порядок Bind работает точно также как &, с другой стороны похож на функцию fmap
+Prelude> :t fmap
+fmap :: Functor f => (a -> b) -> f a -> f b
+Prelude> :t flip fmap
+flip fmap :: Functor f => f a -> (a -> b) -> f b -- fmap не меняет структуру контейнера
+Prelude> :t (>>=)
+(>>=) :: Monad m => m a -> (a -> m b) -> m b     -- bind может изменять сруктуру контейнера
+
+
+**
+Реализуйте фукцию bindLog
+
+bindLog :: Log a -> (a -> Log b) -> Log b
+
+которая работает подобно оператору >>= для контекста Log.
+
+GHCi> Log ["nothing done yet"] 0 `bindLog` add1Log
+Log ["nothing done yet","added one"] 1
+
+GHCi> Log ["nothing done yet"] 3 `bindLog` add1Log `bindLog` mult2Log
+Log ["nothing done yet","added one","multiplied by 2"] 8
+
+bindLog :: Log a -> (a -> Log b) -> Log b
+bindLog (Log xs a) f = Log (xs ++ xss) v where
+  Log xss v = f a
+**
+
+
+**
+Реализованные ранее returnLog и bindLog позволяют объявить тип Log представителем класса Monad:
+
+instance Monad Log where
+    return = returnLog
+    (>>=) = bindLog
+
+Используя return и >>=, определите функцию execLoggersList
+
+execLoggersList :: a -> [a -> Log a] -> Log a
+
+которая принимает некоторый элемент, список функций с логированием и возвращает результат последовательного применения всех функций в списке к переданному элементу вместе со списком сообщений, которые возвращались данными функциями:
+
+GHCi> execLoggersList 3 [add1Log, mult2Log, \x -> Log ["multiplied by 100"] (x * 100)]
+Log ["added one","multiplied by 2","multiplied by 100"] 800
+
+execLoggersList :: a -> [a -> Log a] -> Log a
+execLoggersList a xs = foldl (>>=) (return a) xs
+**
+
+-- Дополнительные вспомогательные функции с реализацими по умолчанию в классе типов Monad
+class Monad m where
+  return :: a -> m a
+  (>>=) :: m a -> (a -> m b) -> m b -- произносят bind
+
+  (>>) :: m a -> m b -> m b
+  x >> y = x >>= \_ -> y
+
+  fail :: String -> m a
+  fail s = error s
+
+(=<<) :: Monad m => (a -> m b) -> m a -> m b
+(=<<) = flip (>>=)
+
+(<=<) :: Monad m => (b -> m c) -> (a -> m b) -> a -> m c -- оператор "рыбки" - оператор композиции стрелок клейсли
+(<=<) f g  x = g x >>= f
 -}
