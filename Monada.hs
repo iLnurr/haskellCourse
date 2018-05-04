c68d5713b80edc6b87f7b36711ecb8532b9ca8e4
serbaevilnur <serbaevilnur@gmail.com>
2018-05-04 14:09:58 +0500

    Functor

--- Monada.hs
+++ Monada.hs
@@ -287,4 +287,84 @@ Prelude> runIdentity goWrap5
 Prelude> :t goWrap5
 goWrap5 :: Identity (Integer, Integer) -- здесь указан тип возвращаемого контейнера
 
+
+-- Monada Maybe
+{-
+class Monad m where
+  return :: a -> m a
+  (>>=) :: m a -> (a -> m b) -> m b  -- оператор связывания
+  (>>) :: m a -> m b -> m b          -- оператор легковесного связывания
+  fail :: String -> m a
+ -}
+
+
+import Prelude hiding (Maybe,Just,Nothing)
+
+data Maybe a = Nothing | Just a
+  deriving (Eq,Show)
+
+instance Monad Maybe where
+  return x = Just x
+
+  (>>=) (Just x) k = k x -- k-стрелка Клейсли
+  (>>=) Nothing _  = Nothing
+
+  (>>) m (Just _) = m
+  (>>) _ Nothing  = Nothing
+
+  fail _ = Nothing
+
+**
+Рассмотрим язык арифметических выражений, которые состоят из чисел, скобок, операций сложения и вычитания. Конструкции данного языка можно представить следующим типом данных:
+
+data Token = Number Int | Plus | Minus | LeftBrace | RightBrace
+    deriving (Eq, Show)
+Реализуйте лексер арифметических выражений. Для начала реализуйте следующую функцию:
+
+asToken :: String -> Maybe Token
+
+Она проверяет, является ли переданная строка числом (используйте функцию isDigit из модуля Data.Char), знаком "+" или "-", открывающейся или закрывающейся скобкой. Если является, то она возвращает нужное значение обёрнутое в Just, в противном случае - Nothing:
+
+GHCi> asToken "123"
+Just (Number 123)
+
+GHCi> asToken "abc"
+Nothing
+
+Далее, реализуйте функцию tokenize:
+
+tokenize :: String -> Maybe [Token]
+Функция принимает на вход строку и если каждое слово является корректным токеном, то она возвращает список этих токенов, завёрнутый в Just. В противном случае возвращается Nothing.
+
+Функция должна разбивать входную строку на отдельные слова по пробелам (используйте библиотечную функцию words). Далее, полученный список строк должен быть свёрнут с использованием функции asToken и свойств монады Maybe:
+
+GHCi> tokenize "1 + 2"
+Just [Number 1,Plus,Number 2]
+
+GHCi> tokenize "1 + ( 7 - 2 )"
+Just [Number 1,Plus,LeftBrace,Number 7,Minus,Number 2,RightBrace]
+
+GHCi> tokenize "1 + abc"
+Nothing
+Обратите внимание, что скобки отделяются пробелами от остальных выражений!
+
+-- data Token = Number Int | Plus | Minus | LeftBrace | RightBrace    
+--     deriving (Eq, Show)
+-- Тип Token уже объявлен, его писать не нужно
+
+import Data.Char
+
+asToken :: String -> Maybe Token
+asToken s | (all isDigit s) = Just $ Number $ read s
+          | s == "+"        = Just Plus
+          | s == "-"        = Just Minus
+          | s == "("        = Just LeftBrace
+          | s == ")"        = Just RightBrace
+          | otherwise       = Nothing
+
+tokenize :: String -> Maybe [Token]
+tokenize input = sequence . map asToken $ words input
+**
+
+
 -}
